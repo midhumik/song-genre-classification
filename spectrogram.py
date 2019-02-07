29ee057069fff40c19470575e115bde8653088bd
midhumik <35134294+midhumik@users.noreply.github.com>
2019-02-07 23:20:22 +0530

    Update spectrogram.py

--- spectrogram.py
+++ spectrogram.py
@@ -4,3 +4,12 @@ import wave
 import pylab
 
 
+def graph_spectrogram(wav_file):
+    sound_info, frame_rate = get_wav_info(wav_file)
+    pylab.figure(num=None, figsize=(19, 12))
+    pylab.subplot(111)
+    pylab.title('spectrogram of %r' % wav_file)
+    pylab.specgram(sound_info, Fs=frame_rate)
+    pylab.savefig('spectrogram - %s' % os.path.splitext(wav_file)[0])
+
+
