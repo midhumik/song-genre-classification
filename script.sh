c88623aaae6eb045ed76ba3524eb7afe42de3a4b
midhumik <35134294+midhumik@users.noreply.github.com>
2019-03-11 20:17:18 +0530

    Update and rename script to script.sh
    To crop every song in in the directory to 30 seconds after trimming the first 10 seconds ,and then divide it to segments of 0.1sec each.

---
+++ script.sh
@@ -0,0 +1,25 @@
+#To crop every song to 30 seconds
+for i in /home/midhun/Documents/Project/pgms/inp/*.mp3;
+  do 
+   name=$(basename "$i")
+#  ffmpeg -ss 10 -t 30 -i "$i" -c copy /home/midhun/Documents/Project/pgms/out/file-$(( ctr+=1 )).mp3;
+   ffmpeg -ss 10 -t 30 -i "$i" -c copy /home/midhun/Documents/Project/pgms/out/cropped/"$name";
+
+
+done
+
+for file in /home/midhun/Documents/Project/pgms/out/cropped/*.mp3
+  do 
+
+  s=$(basename "$file")
+  name=${s%.mp3}
+  echo $name
+  mkdir /home/midhun/Documents/Project/pgms/out/segment/"$name"/
+  ffmpeg -i "$file" -f segment -segment_time 0.1 -c copy /home/midhun/Documents/Project/pgms/out/segment/"$name"/%03d.mp3
+
+done
+
+#for file in /home/midhun/Documents/Project/pgms/out/segment/"$name"/*.mp3
+#do 
+  #librosa3.py
+#done
